39c344b73a2ac40dc68628c8d30f3d1bcfaf39f0
JaewonMin <mjw8523@gmail.com>
2021-01-19 19:26:14 +0900

    Update my code

--- text_processing.py
+++ text_processing.py
@@ -10,31 +10,46 @@ NLP에서 흔히하는 전처리는 소문자 변환, 앞뒤 필요없는 띄어
 
 def normalize(input_string):
     """
-     인풋으로 받는 스트링에서 정규화된 스트링을 반환함
-     아래의 요건들을 충족시켜야함
+    인풋으로 받는 스트링에서 정규화된 스트링을 반환함
+    아래의 요건들을 충족시켜야함
     * 모든 단어들은 소문자로 되어야함
     * 띄어쓰기는 한칸으로 되어야함
     * 앞뒤 필요없는 띄어쓰기는 제거해야함
 
-         Parameters:
-             input_string (string): 영어로 된 대문자, 소문자, 띄어쓰기, 문장부호, 숫자로 이루어진 string
-             ex - "This is an example.", "   EXTRA   SPACE   "
+        Parameters:
+            input_string (string): 영어로 된 대문자, 소문자, 띄어쓰기, 문장부호, 숫자로 이루어진 string
+            ex - "This is an example.", "   EXTRA   SPACE   "
 
-         Returns:
-             normalized_string (string): 위 요건을 충족시킨 정규회된 string
-             ex - 'this is an example.'
+        Returns:
+            normalized_string (string): 위 요건을 충족시킨 정규회된 string
+            ex - 'this is an example.'
 
-         Examples:
-             >>> import text_processing as tp
-             >>> input_string1 = "This is an example."
-             >>> tp.normalize(input_string1)
-             'this is an example.'
-             >>> input_string2 = "   EXTRA   SPACE   "
-             >>> tp.normalize(input_string2)
-             'extra space'
+        Examples:
+            >>> import text_processing as tp
+            >>> input_string1 = "This is an example."
+            >>> tp.normalize(input_string1)
+            'this is an example.'
+            >>> input_string2 = "   EXTRA   SPACE   "
+            >>> tp.normalize(input_string2)
+            'extra space'
     """
-    normalized_string = None
-    return normalized_string
+
+    tmp_input_string = input_string.lower()
+
+    normalized_string = []
+
+    tmp_input_string = tmp_input_string.lstrip()
+    tmp_input_string = tmp_input_string.rstrip()
+
+    size = len(tmp_input_string)
+    for i in range(size):
+        if(tmp_input_string[i] == ' ' and tmp_input_string[i-1] == ' '):
+                continue
+        normalized_string.append(tmp_input_string[i])
+    
+    return ''.join(normalized_string)
+
+
 
 
 def no_vowels(input_string):
@@ -58,5 +73,17 @@ def no_vowels(input_string):
             >>> tp.normalize(input_string2)
             ''W lv Pythn!'
     """
-    no_vowel_string = None
-    return no_vowel_string
+    f_input_string = input_string
+    
+    no_vowel_string = []
+    
+    for i in f_input_string :
+        if ('a' == i or 'A' == i or
+            'e' == i or 'E' == i or
+            'i' == i or 'I' == i or
+            'o' == i or 'O' == i or
+            'u' == i or 'U' == i) : 
+            continue
+        no_vowel_string.append(i)
+        
+    return ''.join(no_vowel_string)
